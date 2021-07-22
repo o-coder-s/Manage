5f341e73bac3f9c6932685ed8068d2300df49aa6
Oussama Sethoum <os.sethoum@gmail.com>
2021-07-23 00:54:08 +0100

    Added folders : Setup Login

--- Manage/Base/Model.cs
+++ Manage/Base/Model.cs
@@ -7,5 +7,9 @@ namespace Manage.Base
     {
         public event PropertyChangedEventHandler PropertyChanged;
         protected void RaiseNotification(String property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+        #region Properties
+
+        #endregion
     }
 }
---
+++ Manage/ViewModels/Login/SignInVM.cs
@@ -0,0 +1,18 @@
+﻿using Manage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage.ViewModels.Login
+{
+    public User User { get; set; }
+    public class SignInVM: Base.ViewModel
+    {
+        public SignInVM()
+        {
+
+        }
+    }
+}
